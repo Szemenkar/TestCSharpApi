fdfb3f9509aacc55c2c6dc1c858cb7f3fad93541
Szemenkar <johannes.szemenkar@hotmail.com>
2024-05-07 13:08:39 +0200

    added features

---
+++ Backend/src/Utils.cs
@@ -0,0 +1,48 @@
+﻿namespace WebApp;
+public static class Utils
+{
+    public static int SumInts(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static Arr CreateMockUsers()
+    {
+        var read = File.ReadAllText(FilePath("json", "mock-users.json"));
+        Arr mockUsers = JSON.Parse(read);
+        Arr successFullyWrittenUsers = Arr();
+        foreach (var user in mockUsers)
+        {
+            var result = SQLQueryOne(
+                @"INSERT INTO users(firstName,lastName,email,password)
+                VALUES($firstName, $lastName, $email, $password)
+            ", user);
+            // If we get an error from the DB then we haven't added the mock users
+            // If not we have so add to the successful list
+            if(!result.HasKey("error"))
+            {
+                // The specification says return the user list without password
+                user.Delete("password");
+                successFullyWrittenUsers.Push(user);
+            }
+        }
+        return successFullyWrittenUsers;
+    }
+
+    public static bool IsPasswordGoodEnough(string password)
+    {
+        List<bool> bools = new List<bool>();
+
+        bools.Add(password.Any(char.IsLower));
+        bools.Add(password.Any(char.IsNumber));
+        bools.Add(password.Any(char.IsUpper));
+        bools.Add(password.Count() > 7);
+        bools.Add(password.Any(ch => !char.IsLetterOrDigit(ch)));
+
+        if (bools.Contains(false))
+        {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ Backend/unit-tests/UtilsTest.cs
@@ -0,0 +1,46 @@
+﻿namespace WebApp;
+public class UtilsTest(Xlog Console)
+{
+    [Fact]
+    public void TestSumInt()
+    {
+        Assert.Equal(12, Utils.SumInts(7, 5));
+        Assert.Equal(-3, Utils.SumInts(6, -9));
+    }
+
+    [Fact]
+    public void TestCreateMockUsers()
+    {
+        // Read all mock users from the JSON file
+        var read = File.ReadAllText(FilePath("json", "mock-users.json"));
+        Arr mockUsers = JSON.Parse(read);
+
+        // Get all users from the database
+        Arr usersInDb = SQLQuery("SELECT email FROM users");
+        Arr emailsInDb = usersInDb.Map(user => user.email);
+
+        // Only keep the mock users not already in db
+        Arr mockUsersNotInDb = mockUsers.Filter(mockUser => !emailsInDb.Contains(mockUser.email));
+
+        // Assert that the CreateMockUsers only return newly created users in the db
+
+        var result = Utils.CreateMockUsers();
+
+        // Assert that the CreateMockUsers only return
+        // newly created users in the db
+        Console.WriteLine($"The test expected that {mockUsersNotInDb.Length} users should be added.");
+        Console.WriteLine($"And {result.Length} users were added.");
+        Console.WriteLine("The test also asserts that the users added are equivalent (the same) to the expected users!");
+        Assert.Equivalent(mockUsersNotInDb, result);
+        Console.WriteLine("The test passed!");
+    }
+
+    [Fact]
+    public void TestIsPasswordGoodEnough()
+    {
+        Assert.False(Utils.IsPasswordGoodEnough("12345678"));
+        Assert.False(Utils.IsPasswordGoodEnough("12345qwe"));
+        Assert.False(Utils.IsPasswordGoodEnough("awseQWE21q3"));
+        Assert.True(Utils.IsPasswordGoodEnough("afsW12???"));
+    }
+}
