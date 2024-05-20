572ad92f6f8514772cbe6675ebaea151f0855b99
Szemenkar <johannes.szemenkar@hotmail.com>
2024-05-20 10:21:28 +0200

    added for interface testing

--- Backend/src/Utils.cs
+++ Backend/src/Utils.cs
@@ -1,16 +1,46 @@
 ﻿namespace WebApp;
 public static class Utils
 {
-    public static int SumInts(int a, int b)
+    private static readonly Arr mockUsers = JSON.Parse(
+    File.ReadAllText(FilePath("json", "mock-users.json")));
+
+    private static readonly Arr badWords = ((Arr)JSON.Parse(
+    File.ReadAllText(FilePath("json", "badwords.json"))
+    )).Sort((a, b) => ((string)b).Length - ((string)a).Length);
+
+    public static bool IsPasswordGoodEnough(string password)
     {
-        return a + b;
+        List<bool> bools = new List<bool>();
+
+        bools.Add(password.Count() > 7);
+        bools.Add(password.Any(char.IsDigit));
+        bools.Add(password.Any(char.IsLower));
+        bools.Add(password.Any(char.IsUpper));
+        bools.Add(password.Any(x => !char.IsLetterOrDigit(x)));
+
+        if (bools.Contains(false))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public static string RemoveBadWords(string comment, string replaceWith = "---")
+    {
+        comment = " " + comment;
+        replaceWith = " " + replaceWith + "$1";
+        badWords.ForEach(bad =>
+        {
+            var pattern = @$" {bad}([\,\.\!\?\:\; ])";
+            comment = Regex.Replace(
+                comment, pattern, replaceWith, RegexOptions.IgnoreCase);
+        });
+        return comment[1..];
     }
 
     public static Arr CreateMockUsers()
     {
-        var read = File.ReadAllText(FilePath("json", "mock-users.json"));
-        Arr mockUsers = JSON.Parse(read);
-        Arr successFullyWrittenUsers = Arr();
+        Arr successfullyWrittenUsers = Arr();
         foreach (var user in mockUsers)
         {
             var result = SQLQueryOne(
@@ -18,31 +48,65 @@ public static class Utils
                 VALUES($firstName, $lastName, $email, $password)
             ", user);
             // If we get an error from the DB then we haven't added the mock users
-            // If not we have so add to the successful list
+            // If not we have to add to the successful list
             if(!result.HasKey("error"))
             {
                 // The specification says return the user list without password
                 user.Delete("password");
-                successFullyWrittenUsers.Push(user);
+                successfullyWrittenUsers.Push(user);
             }
         }
-        return successFullyWrittenUsers;
+        return successfullyWrittenUsers;
     }
 
-    public static bool IsPasswordGoodEnough(string password)
+    public static Obj CountDomainsFromUserEmails()
     {
-        List<bool> bools = new List<bool>();
+        var emailDomain = Obj();
 
-        bools.Add(password.Any(char.IsLower));
-        bools.Add(password.Any(char.IsNumber));
-        bools.Add(password.Any(char.IsUpper));
-        bools.Add(password.Count() > 7);
-        bools.Add(password.Any(ch => !char.IsLetterOrDigit(ch)));
+        var allEmailsInDb = SQLQuery("SELECT email FROM users");
 
-        if (bools.Contains(false))
+        foreach (var domain in allEmailsInDb)
         {
-            return false;
+            var domains = RetrieveDomainName(domain);
+            if (domains.HasKey())
+            {
+                emailDomain[domains]++;
+            }
+            else
+            {
+                emailDomain[domains] = 1;
+            }
         }
-        return true;
+        return emailDomain;
+    }
+
+    public static string RetrieveDomainName(string email)
+    {
+        var domainLocation = email.IndexOf('@');
+        return email.Substring(domainLocation + 1, email.Length - domainLocation - 1);
+    }
+
+    public static Arr RemoveMockUsers()
+    {
+        Arr successfullyRemovedUsers = Arr();
+
+        Arr allUsersInDb = SQLQuery("SELECT email FROM users");
+        Arr allEmailsInDb = allUsersInDb.Map(user => user.email);
+
+        Arr mockUsersInDb = mockUsers.Filter(mockUser => allEmailsInDb.Contains(mockUser.email));
+        
+        foreach (var email in mockUsersInDb)
+        {
+            var result = SQLQueryOne(
+            @"DELETE FROM users
+            WHERE users.email = $email", email
+        );
+
+        if(!result.HasKey("error"))
+        {
+            successfullyRemovedUsers.Push(email);
+        }
+        }
+        return successfullyRemovedUsers;
     }
 }
--- Backend/unit-tests/UtilsTest.cs
+++ Backend/unit-tests/UtilsTest.cs
@@ -1,20 +1,33 @@
 ﻿namespace WebApp;
 public class UtilsTest(Xlog Console)
 {
-    [Fact]
-    public void TestSumInt()
+    private static readonly Arr mockUsers = JSON.Parse(
+    File.ReadAllText(FilePath("json", "mock-users.json")));
+    
+    [Theory]
+    [InlineData("afsW12???", true)]
+    [InlineData("12345678", false)]
+    [InlineData("awseQWE21q3", false)]
+    [InlineData("12345qwe", false)]
+    public void TestIsPasswordGoodEnough(string password, bool expected)
     {
-        Assert.Equal(12, Utils.SumInts(7, 5));
-        Assert.Equal(-3, Utils.SumInts(6, -9));
+        Assert.Equal(expected, Utils.IsPasswordGoodEnough(password));
+    }
+
+    [Theory]
+    [InlineData(
+        "---",
+        "Hello, go fuck yourself. This is fucking real, no shells!",
+        "Hello, go --- yourself. This is --- real, no shells!"
+    )]
+    public void TestRemoveBadWords(string replaceWith, string original, string expected)
+    {
+        Assert.Equal(expected, Utils.RemoveBadWords(original, replaceWith));
     }
 
     [Fact]
     public void TestCreateMockUsers()
     {
-        // Read all mock users from the JSON file
-        var read = File.ReadAllText(FilePath("json", "mock-users.json"));
-        Arr mockUsers = JSON.Parse(read);
-
         // Get all users from the database
         Arr usersInDb = SQLQuery("SELECT email FROM users");
         Arr emailsInDb = usersInDb.Map(user => user.email);
@@ -30,17 +43,32 @@ public class UtilsTest(Xlog Console)
         // newly created users in the db
         Console.WriteLine($"The test expected that {mockUsersNotInDb.Length} users should be added.");
         Console.WriteLine($"And {result.Length} users were added.");
-        Console.WriteLine("The test also asserts that the users added are equivalent (the same) to the expected users!");
         Assert.Equivalent(mockUsersNotInDb, result);
         Console.WriteLine("The test passed!");
     }
 
     [Fact]
-    public void TestIsPasswordGoodEnough()
+    public void TestRemoveMockUsers()
     {
-        Assert.False(Utils.IsPasswordGoodEnough("12345678"));
-        Assert.False(Utils.IsPasswordGoodEnough("12345qwe"));
-        Assert.False(Utils.IsPasswordGoodEnough("awseQWE21q3"));
-        Assert.True(Utils.IsPasswordGoodEnough("afsW12???"));
+        Arr usersInDb = SQLQuery("SELECT email FROM users");
+        Arr emailsInDb = usersInDb.Map(user => user.email);
+
+        Arr mockUsersInDb = mockUsers.Filter(mockUser => emailsInDb.Contains(mockUser.email));
+
+        var result = Utils.RemoveMockUsers();
+
+        Console.WriteLine($"The test expected that {mockUsersInDb.Length} users should be removed.");
+        Console.WriteLine($"And {result.Length} users were removed.");
+        Assert.Equivalent(mockUsersInDb, result);
+        Console.WriteLine("The test passed!");
+    }
+
+    [Fact]
+    public void TestCountDomainsFromUserEmails()
+    {
+
+        var result = Utils.CountDomainsFromUserEmails();
+
+        Assert.Equivalent(, result);
     }
 }
